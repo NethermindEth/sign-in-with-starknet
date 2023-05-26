f79c4b4f281739b0fc62af65e243b3f2f4a639b8
Fawad <153737+FawadHa1der@users.noreply.github.com>
2023-05-26 15:58:40 -0400

    next js config update

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   reactStrictMode: true,
   // Use the CDN in production and localhost for development.
-  assetPrefix: process.env.NODE_ENV === "production" ? 'http://143.42.2.9' : undefined,
+  assetPrefix: process.env.NODE_ENV === "production" ? 'http://143.42.2.9:3000' : undefined,
 };
