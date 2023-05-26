ba448c85a0316ed3ffbcabe71e85e071a1875284
Fawad <153737+FawadHa1der@users.noreply.github.com>
2023-05-26 15:46:33 -0400

    adding asset prefix to the front end next app

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,5 @@
-/** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-}
+  // Use the CDN in production and localhost for development.
+  assetPrefix: isProd ? 'http://143.42.2.9' : undefined,
+};
