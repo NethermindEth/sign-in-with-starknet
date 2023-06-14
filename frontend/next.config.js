d668bf5973be68ea1969c1306e67af843262367b
Fawad <153737+FawadHa1der@users.noreply.github.com>
2023-06-14 17:17:08 -0400

    fix : for production set asset prefix

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   reactStrictMode: false, // process.env.NODE_ENV === "production" ? false : true, // this useEffect  to be called twice thats why its set to false
   // Use the CDN in production and localhost for development.
-  assetPrefix: process.env.NODE_ENV === "production" ? 'http://143.42.2.9:3000' : undefined,
+  assetPrefix: process.env.NODE_ENV === "production" ? 'https://siws.nethermind.io/' : undefined,
 };
